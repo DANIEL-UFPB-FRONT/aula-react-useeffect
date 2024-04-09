b1da1a8d42888f337135167c3726a3bfdc1a8909
Daniel Faustino <daniel@dcx.ufpb.br>
2024-04-08 23:13:49 -0300

    (feat) move code to work with vite

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Message from './components/Message';
 
-
-
 function App() {
   const [show, setShow] = useState(false)    
   return (
     <div id="container">
         <h2>The useEffect Example</h2>
         <div id='check-container'>
-          <input id='check' type='checkbox' onClick={e => setShow(e.target.checked)}></input>
+          <input id='check' type='checkbox'  onClick={e => setShow(e.target.checked)}></input>
           <label htmlFor='check'>Show message </label>
         </div>
         {show && <Message />}
